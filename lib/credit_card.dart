b6ca146b48a89ad93bd45196a9034d9cfd3c3f30
Subramanya Chakravarthy <chanty.chakravarthy@gmail.com>
2018-12-15 16:53:04 +0530

    add some code

--- lib/credit_card.dart
+++ lib/credit_card.dart
@@ -1,7 +1,45 @@
 library credit_card;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+enum CreditCardValues {
+  NUMBER,
+  EXPIRY,
+  CVC,
+  TYPE,
+  NAME,
+}
+
+const Object PlaceHolder = {
+  "number": "1234 5678 1234 5678",
+  "expiry": "MM/YY",
+  "cvc": "CVC"
+};
+
+class CreditCard extends StatefulWidget{
+
+  final CreditCardValues focusedValue;
+  final String brand;
+  final String name;
+  final String number;
+  final String expiry;
+  final String cvc;
+  final Object placeHolder;
+
+  const CreditCard({Key key, this.focusedValue, this.brand, this.name, this.number, this.expiry, this.cvc, this.placeHolder = PlaceHolder}) : super(key: key);
+
+
+  @override
+  State<StatefulWidget> createState() {
+    return _CreditCardState();
+  }
+}
+
+class _CreditCardState extends State<CreditCard> with SingleTickerProviderStateMixin {
+  @override
+  Widget build(BuildContext context) {
+    return Card(child: Image.asset('assets/card-front.png'), );
+  }
+
 }
